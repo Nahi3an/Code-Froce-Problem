7fce6d633665b0769aba33b42307b7e536abace5
Nahi3an <nafisnahiyan321@gmail.com>
2020-08-21 01:33:06 +0600

    Math composite number problem

--- Design_Tutorial_Learn_from_Math.cpp
+++ Design_Tutorial_Learn_from_Math.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 using namespace std;
-
-
 int main(){
-
-
 int n,x = 4,y;
 
 cin>>n;
@@ -13,27 +9,18 @@ if(n%2==0){
     y = n - x;
 
 }else{
-
     int i = 0;
     while(i<n){
-
-        y = n - x;
-
+     y = n - x;
         if(y%3==0){
-
             break;
         }
         else{
-
             x = x+2;
         }
-
         i++;
     }
-
-
 }
-
 
 cout<<x<<" "<<y<<endl;
 
