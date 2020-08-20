30fa22242ba08aecf63410bb50c961d0dcf88211
Nahi3an <nafisnahiyan321@gmail.com>
2020-08-21 01:29:54 +0600

    Composite number

---
+++ Design_Tutorial_Learn_from_Math.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+
+int n,x = 4,y;
+
+cin>>n;
+
+if(n%2==0){
+    y = n - x;
+
+}else{
+
+    int i = 0;
+    while(i<n){
+
+        y = n - x;
+
+        if(y%3==0){
+
+            break;
+        }
+        else{
+
+            x = x+2;
+        }
+
+        i++;
+    }
+
+
+}
+
+
+cout<<x<<" "<<y<<endl;
+
+return 0;
+
+
+
+
+
+}
