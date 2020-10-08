58e8630725c645f79476e665629b922d0dd4242d
Nafis Nawal Nahiyan <nafisnahiyan321@gmail.com>
2020-10-09 01:37:19 +0600

    Solved by seeing code, shame

---
+++ Array with Odd Sum.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int t;
+cin>>t;
+
+while(t--){
+    int n;
+    cin>>n;
+
+    int  arr[n];
+    int odd=0,even=0;
+    for(int i=0; i<n; i++){
+
+        cin>>arr[i];
+
+        if(arr[i]%2==0) even++;
+        else odd++;
+
+    }
+
+    if(odd == 0 || (even==0 && n%2==0)){
+
+        cout<<"NO"<<endl;
+
+    }else{
+
+        cout<<"YES"<<endl;
+
+    }
+
+
+}
+
+
+
+ return 0;
+
+
+
+
+}
