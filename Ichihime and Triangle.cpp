f5f21503b5af49bb4bb3d61c7324ae63542129f0
Nafis Nawal Nahiyan <nafisnahiyan321@gmail.com>
2020-10-08 18:28:58 +0600

    Should learn to think less critically

---
+++ Ichihime and Triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int a,b,c,d;
+
+        cin>>a>>b>>c>>d;
+
+        int x,y,z;
+        x = b;
+        y = c;
+        z = c;
+
+        cout<<x<<" "<<y<<" "<<z<<endl;
+
+    }
+}
