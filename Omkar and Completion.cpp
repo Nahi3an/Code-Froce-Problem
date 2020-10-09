04bfb62747ad3c516057d555fdaf68b25c181814
Nafis Nawal Nahiyan <nafisnahiyan321@gmail.com>
2020-10-10 01:31:07 +0600

    Nice tricky problem

---
+++ Omkar and Completion.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int a;
+        cin>>a;
+
+        int num = 2;
+        for(int i=0; i<a; i++){
+
+            cout<<num<<" ";
+
+
+        }
+        cout<<endl;
+    }
+
+
+
+
+
+
+
+}
