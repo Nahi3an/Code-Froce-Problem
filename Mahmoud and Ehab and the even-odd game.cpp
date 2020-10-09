dd17868173783a04f728ad37d0513cf564eab082
Nafis Nawal Nahiyan <nafisnahiyan321@gmail.com>
2020-10-10 01:52:51 +0600

    Easy problem

---
+++ Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int a;
+    cin>>a;
+
+    if(a%2==0){
+
+        cout<<"Mahmud"<<endl;
+    }else{
+
+        cout<<"Ehab"<<endl;
+
+    }
+
+    return 0;
+
+
+
+}
