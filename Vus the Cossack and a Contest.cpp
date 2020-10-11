9cf72f7f35de1272be5a58adb97336cbdbcb1917
Nafis Nawal Nahiyan <nafisnahiyan321@gmail.com>
2020-10-11 19:54:43 +0600

    Easy problem

---
+++ Vus the Cossack and a Contest.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+
+    int participents,pen,notebook;
+
+    cin>>participents>>pen>>notebook;
+
+    if(pen>=participents && notebook >= participents){
+
+        cout<<"Yes"<<endl;
+
+    }else{
+
+        cout<<"No"<<endl;
+
+
+    }
+
+    return 0;
+}
